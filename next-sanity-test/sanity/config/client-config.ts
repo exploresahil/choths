2b06c4ebd470b3fa811da7f788a6ad79e75f571b
Sahil Satpute <116795291+exploresahil@users.noreply.github.com>
2023-06-05 17:36:16 +0530

    uploaded

---
+++ next-sanity-test/sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "03oed9zq",
+  dataset: "production",
+  apiVersion: "2023-06-05",
+};
+
+export default config;
---
+++ next-sanity-test/sanity/schemas/project-schema.ts
@@ -0,0 +1,17 @@
+const project = {
+  name: "project",
+  title: "Projects",
+  type: "document",
+  options: {
+    singleton: true, // Define the document as a singleton
+  },
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+  ],
+};
+
+export default project;

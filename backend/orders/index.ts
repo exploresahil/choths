9c88686f1ec5e464547dae6618d4d2a9312e42e9
parag koche <paragkoche0@gmail.com>
2023-06-25 14:15:48 +0530

    add backend types

--- backend/orders/index.ts
+++ backend/orders/index.ts
@@ -1,3 +1,16 @@
 import SuperBase from "@/backend/supabase";
 
 // TODO make all CRUD op for User  Orders data with mapping user
+//those functions use to order
+export const makeOrder = () => {
+
+}
+export const CancelOrder = () => { }
+export const UpdateOrder = () => { }
+export const getOrders = () => { }
+export const getOrdersId = () => { }
+
+//those functions use to cart
+export const AddCartOrder = () => { }
+export const RemoveCartOrder = () => { }
+export const UpdateCartOrder = () => { }

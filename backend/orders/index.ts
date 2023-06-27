9e5507268a233b6f2e0d571ae504bd3c2ef58e48
parag koche <paragkoche0@gmail.com>
2023-06-27 09:55:18 +0530

    make Oder Fun complete with DOC

--- backend/orders/index.ts
+++ backend/orders/index.ts
@@ -1,16 +1,53 @@
 import SuperBase from "@/backend/supabase";
+import { Database } from "@/backend/types"
 
 // TODO make all CRUD op for User  Orders data with mapping user
 //those functions use to order
-export const makeOrder = () => {
-
+/**
+ * 
+ * @param {string} product_id
+ * @param {Database['public']['Tables']['USER']['Row']} user 
+ * @returns {Promise<any>} Order
+ * ```ts
+ * //how to use
+ * await makeOrder(product_id,{...userData}).then((data)=>{
+ * //handel data
+ * },(error)=>{
+ * //handel error
+ * })
+ * 
+ * ```
+ */
+export const makeOrder = async (product_id: string, user: Database['public']['Tables']['USER']['Row']): Promise<any> => {
+    return await SuperBase.from("Order").insert({
+        product: product_id,
+        user: user.id,
+        cancel: false
+    }).select()
 }
-export const CancelOrder = () => { }
-export const UpdateOrder = () => { }
-export const getOrders = () => { }
-export const getOrdersId = () => { }
+export const CancelOrder = (order_id: string) => { }
+export const UpdateOrder = (order_id: string, updateObject: Database['public']['Tables']['Order']['Update']) => { }
+export const getOrders = () => {
+    return SuperBase.from("Order").select("*")
+}
+export const getOrdersId = (order_id: string) => {
+    return SuperBase.from("Order").select("*").eq("id", order_id)
+}
 
 //those functions use to cart
-export const AddCartOrder = () => { }
-export const RemoveCartOrder = () => { }
-export const UpdateCartOrder = () => { }
+export const AddCartOrder = (product_id: string, user_id: string, how_many: number) => {
+
+}
+export const RemoveCartOrder = (
+    id: string
+) => {
+    return SuperBase.from("Cart").delete().eq("id", id)
+
+}
+export const UpdateCartOrder = (
+    id: string,
+    data: any
+) => {
+
+    return SuperBase.from("Cart").update(data).eq("id", id).select("*")
+}

bd2aa0b29f9c01b6c7623a811bf0d2118b0f2891
Latte-47 <115649083+Latte-47@users.noreply.github.com>
2023-06-28 15:05:33 +0530

    Blogs page returned to original state. Blogs filters need to be implemented.

--- app/(client)/blogs/page.tsx
+++ app/(client)/blogs/page.tsx
@@ -4,21 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { PortableText } from "@portabletext/react";
 
-import {
-  getBlogs,
-  getUpcycleBlogs,
-  getIndustryBlogs,
-  getBtsBlogs,
-  getShopBlogs,
-  getFeatured,
-} from "@/sanity/sanity-utils";
-import {
-  AllBlogs,
-  UpcycleBlogs,
-  BtsBlogs,
-  ShopBlogs,
-  IndustryBlogs,
-} from "@/components/client/BlogsList";
+import { getBlogs, getFeatured } from "@/sanity/sanity-utils";
 import Newsletter from "@/components/client/NewsLetter";
 import FAQs from "@/components/client/FAQs";
 
@@ -27,25 +13,10 @@ import {
   CategoriesWatermarkCenter,
   CategoriesWatermarkOuter,
 } from "@/components/icons/Icons";
-import { useState } from "react";
 
 async function Blogs() {
-  const [upcycle, setUpcycle] = useState(false);
-  const [industry, setIndustry] = useState(false);
-  const [bts, setBts] = useState(false);
-  const [shop, setShop] = useState(false);
-
-  const allBlogs = await getBlogs();
-  const upcycleBlogs = await getUpcycleBlogs();
-  const industryBlogs = await getIndustryBlogs();
-  const btsBlogs = await getBtsBlogs();
-  const shopBlogs = await getShopBlogs();
+  const blogs = await getBlogs();
   const featured = await getFeatured();
-
-  console.log(upcycle);
-  console.log(industry);
-  console.log(bts);
-  console.log(shop);
 
   return (
     <div className="blogs-main">
@@ -86,70 +57,29 @@ async function Blogs() {
       ))}
       <div className="blogs-filter-section">
         <div className="filter-container">
-          <button
-            type="button"
-            onClick={() => {
-              setUpcycle(!upcycle);
-              setIndustry(false);
-              setBts(false);
-              setShop(false);
-            }}
-          >
+          <button type="button">
             <h3>UPCYCLING 101</h3>
           </button>
         </div>
         <div className="filter-container">
-          <button
-            type="button"
-            onClick={() => {
-              setUpcycle(false);
-              setIndustry(!industry);
-              setBts(false);
-              setShop(false);
-            }}
-          >
+          <button type="button">
             <h3>INDUSTRY NEWNESS</h3>
           </button>
         </div>
         <div className="filter-container">
-          <button
-            type="button"
-            onClick={() => {
-              setUpcycle(false);
-              setIndustry(false);
-              setBts(!bts);
-              setShop(false);
-            }}
-          >
+          <button type="button">
             <h3>BEHIND THE SCENES</h3>
           </button>
         </div>
         <div className="filter-container">
-          <button
-            type="button"
-            onClick={() => {
-              setUpcycle(false);
-              setIndustry(false);
-              setBts(false);
-              setShop(!shop);
-            }}
-          >
+          <button type="button">
             <h3>SHOP HACKS</h3>
           </button>
         </div>
       </div>
 
-      {upcycle == false &&
-        industry == false &&
-        bts == false &&
-        shop == false && <AllBlogs />}
-      {upcycle == true && <UpcycleBlogs />}
-      {industry == true && <IndustryBlogs />}
-      {bts == true && <BtsBlogs />}
-      {shop == true && <ShopBlogs />}
-
-      {/* <div className="blogs-articles-section">
-        {allBlogs.map((blog) => (
+      <div className="blogs-articles-section">
+        {blogs.map((blog) => (
           <div key={blog._id} className="articles-container">
             <div className="articles-img-container">
               {blog.image && (
@@ -180,7 +110,7 @@ async function Blogs() {
             </div>
           </div>
         ))}
-      </div> */}
+      </div>
 
       <div className="blogs-faqs-section">
         <FAQs />

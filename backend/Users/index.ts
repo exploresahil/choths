38d79d566a722c1aff81354cb5afd0272a1abea2
parag koche <paragkoche0@gmail.com>
2023-06-28 09:27:45 +0530

    backend completed ❤️❤️

--- backend/Users/index.ts
+++ backend/Users/index.ts
@@ -1,8 +1,21 @@
 import SuperBase from "@/backend/supabase";
+import { Database } from "../types";
 
-// TODO make all CRUD op for User for additional data with mapping user
+export const addUserData = (
+    user: Database['public']['Tables']['USER']['Insert']
+) => {
 
-export const addUserData = () => { }
-export const UpdateUserData = () => { }
-export const DeleteUserData = () => { }
-export const getUserData = () => { }
+    return SuperBase.from("USER").insert(user)
+}
+export const UpdateUserData = (
+    user_id: string,
+    user: Database['public']['Tables']['USER']['Update']
+) => {
+    return SuperBase.from("USER").update(user).eq("id", user_id)
+}
+export const DeleteUserData = (user_id: string) => {
+    return SuperBase.from("USER").delete().eq("id", user_id)
+}
+export const getUserData = (user_id: string) => {
+    return SuperBase.from("USER").select("*").eq("id", user_id)
+}

6d14350e03abb37b6b7bb587e70d238d5be2a87b
Sahil Satpute <explore.sahil1@gmail.com>
2023-07-04 18:00:05 +0530

    Updated Menu Links

--- components/client/NavLinks.tsx
+++ components/client/NavLinks.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/display-name */
 /* eslint-disable import/no-anonymous-default-export */
-import Link from "next/link";
-
 type NavLinksProps = {
   title: string;
   pageLink: string;
@@ -12,9 +10,9 @@ type NavLinksProps = {
 export default ({ title, pageLink, classTitle, onClick }: NavLinksProps) => {
   return (
     <li>
-      <Link href={pageLink} className={classTitle} onClick={onClick}>
+      <a href={pageLink} className={classTitle} onClick={onClick}>
         {title}
-      </Link>
+      </a>
     </li>
   );
 };

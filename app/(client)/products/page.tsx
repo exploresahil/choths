f2e9e54285c09633281a6f66c2b6dbc94f5fb17c
Latte-47 <115649083+Latte-47@users.noreply.github.com>
2023-07-06 17:45:20 +0530

    Products Category functionality implemented.

--- app/(client)/products/page.tsx
+++ app/(client)/products/page.tsx
@@ -3,6 +3,7 @@
 import Filter from "@/components/client/Filter";
 import Image from "next/image";
 
+import BlogsArrow from "@/components/icons/BlogsArrow";
 import productImg from "@/public/assets/images/products/product-img.png";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -14,6 +15,7 @@ import { category } from "@/sanity/types/Category";
 const Products = () => {
   const [products, setProducts] = useState<products[]>([]);
   const [categories, setCategories] = useState<category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("view all");
 
   useEffect(() => {
     async function fetchProducts() {
@@ -28,43 +30,99 @@ const Products = () => {
 
     fetchProducts();
     fetchCategories();
-  }, []);
+  }, [selectedCategory]);
+
+  const handleCategoryClick = (selectedCategory: any) => {
+    setSelectedCategory(selectedCategory);
+  };
+
+  console.log(selectedCategory);
 
   return (
     <div className="products-main">
       <Filter />
       <div className="products-container">
         <div className="sidebar">
-          <ul>
+          <ul className="category-container">
+            <li
+              className={`${selectedCategory === "view all" ? "active" : ""}`}
+              onClick={() => {
+                handleCategoryClick("view all");
+              }}
+            >
+              view all
+              <BlogsArrow />
+            </li>
             {categories.map((category) => (
-              <li key={category._id}>{category.name}</li>
+              <li
+                className={`${
+                  selectedCategory === `${category.name}` ? "active" : ""
+                }`}
+                key={category._id}
+                onClick={() => {
+                  handleCategoryClick(category.name);
+                }}
+              >
+                {category.name}
+                <BlogsArrow />
+              </li>
             ))}
           </ul>
         </div>
         <div className="products">
           <div className="products-grid">
-            {products.map((product) => (
-              <Link
-                key={product._id}
-                href={`/products/${product.slug}`}
-                className="product"
-              >
-                <div className="img-container">
-                  {product.images && (
-                    <Image
-                      fill
-                      src={product.images[0].url}
-                      style={{ objectFit: "cover" }}
-                      alt={product.slug}
-                    />
-                  )}
-                </div>
-                <div className="product-info">
-                  <h3>{product.name}</h3>
-                  <p>RS.{product.price}</p>
-                </div>
-              </Link>
-            ))}
+            {products.map((product) => {
+              if (selectedCategory == "view all") {
+                return (
+                  <Link
+                    key={product._id}
+                    href={`/products/${product.slug}`}
+                    className="product"
+                  >
+                    <div className="img-container">
+                      {product.images && (
+                        <Image
+                          fill
+                          src={product.images[0].url}
+                          style={{ objectFit: "cover" }}
+                          alt={product.slug}
+                        />
+                      )}
+                    </div>
+                    <div className="product-info">
+                      <h3>{product.name}</h3>
+                      <p>RS.{product.price}</p>
+                    </div>
+                  </Link>
+                );
+              } else if (
+                selectedCategory == product.category.name &&
+                product.category.name == product.category.name
+              ) {
+                return (
+                  <Link
+                    key={product._id}
+                    href={`/products/${product.slug}`}
+                    className="product"
+                  >
+                    <div className="img-container">
+                      {product.images && (
+                        <Image
+                          fill
+                          src={product.images[0].url}
+                          style={{ objectFit: "cover" }}
+                          alt={product.slug}
+                        />
+                      )}
+                    </div>
+                    <div className="product-info">
+                      <h3>{product.name}</h3>
+                      <p>RS.{product.price}</p>
+                    </div>
+                  </Link>
+                );
+              }
+            })}
           </div>
         </div>
         <div className="blank" />
